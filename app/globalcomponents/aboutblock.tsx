904debe99a210823152efafedca0b9ba4bd7fa50
danieldavemena <danieldanieldanieldave@gmail.com>
2025-03-30 21:56:05 +0800

    modified:   app/globalcomponents/aboutblock.tsx 	new file:   app/globalcomponents/line.tsx 	modified:   app/index/components/banner.tsx

--- app/globalcomponents/aboutblock.tsx
+++ app/globalcomponents/aboutblock.tsx
@@ -1,15 +1,39 @@
 import React from "react";
+import Line from "./line";
 
 const aboutblock = () => {
   return (
-    <div className="about-block content bg-[#ffffff16] w-[1000px] h-[600px] rounded-[25px] p-5">
-      <p className="text-justify text-xl">
+    <div className="about-block content grid grid-cols-3 grid-rows-3 [&>*]:m-2 bg-[#ffffff16] text-zinc-200 w-[1000px] h-[600px] rounded-[25px] p-10">
+      <p className="col-span-1 bg-amber-300 row-span-2 "></p>
+      <p className="text-justify text-lg col-span-2 ">
         Greeting! My name Daniel Dave Meña. A developer based in the
         Philippines. I specialize in building scalable, high-performance web
         applications that prioritize efficiency, usability, and maintainability.
         My approach focuses on clean, efficient code, seamless user experiences,
         and architectures that handle scale without compromising performance.
       </p>
+      <div className="col-span-2  row-span-2">
+        <Line />
+        <p className="mt-2 font-bold tracking-widest text-center text-[20px]">
+          Education
+        </p>
+        <div className="mx-2 mt-3 ">
+          <div className="flex flex-row w-full">
+            <p>Sumulong College of Arts and Sciences</p>
+            <div className="text-right mr-0 ml-auto text-zinc-400 ">
+              <p>Senior High School</p>
+              <p>STEM</p>
+            </div>
+          </div>
+          <div className="flex flex-row w-full mt-2">
+            <p>University of Rizal System</p>
+            <div className="text-right mr-0 ml-auto text-zinc-400 ">
+              <p>Undergraduate</p>
+              <p>BS Computer Engineering</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ app/globalcomponents/line.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const line = () => {
+  return <div className="h-1 rounded-full w-full bg-[#ffffff16]"></div>;
+};
+
+export default line;
