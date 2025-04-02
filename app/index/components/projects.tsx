f62e5dd114b0999001a322acc34b8aa3970dd9a1
danieldavemena <danieldanieldanieldave@gmail.com>
2025-04-03 00:56:31 +0800

    modified:   app/index/components/projects.tsx

--- app/index/components/projects.tsx
+++ app/index/components/projects.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ProjectContainer from "~/globalcomponents/projectcontainer";
-import InstaClone from "../../../public/images/instagram-clone-preview.png";
-import Pixmeet from "../../../public/images/pixmeet-preview.png";
+import InstaClone from "/images/instagram-clone-preview.png";
+import Pixmeet from "/images/pixmeet-preview.png";
 
 const projects = () => {
   const projectOne = [
