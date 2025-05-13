ea38e64c9c9cfee21b32f1703dace453f2cee5b4
danieldavemena <danieldanieldanieldave@gmail.com>
2025-05-13 21:32:11 +0800

    modified:   app/globalcomponents/aboutblock.tsx

--- app/globalcomponents/aboutblock.tsx
+++ app/globalcomponents/aboutblock.tsx
@@ -3,22 +3,22 @@ import Line from "./line";
 
 const aboutblock = () => {
   return (
-    <div className="content grid md:grid-cols-3 md:grid-rows-2 grid-cols-2 grid-rows-3 [&>*]:m-2 bg-[#ffffff16] text-zinc-200 md:w-[1000px] md:h-[450px] w-[300px] h-[790px] rounded-[25px] p-10">
+    <div className="content grid md:grid-cols-3 md:grid-rows-2 grid-cols-2 grid-rows-3 [&>*]:m-2 bg-[#ffffff16] text-zinc-200 md:w-[1000px] md:h-[450px] w-[300px] h-[790px] rounded-[25px] md:p-10 p-5">
       <div className="  bg-gradient-to-r from-gray-500 to-[#152238] md:col-span-1 md:row-span-2 col-span-2 row-span-1 rounded-xl ">
         <img
-          className="md:scale-110 md:mt-3.5 scale-95"
+          className="md:scale-110 md:mt-3.5 scale-85 -mt-4"
           src="/images/Portrait.png"
           alt="Dave"
         />
       </div>
-      <p className="text-justify md:text-lg text-[12.5px] md:col-span-2 col-span-2 row-span-1 ">
+      <p className="text-justify md:text-lg text-[13px] md:pt-0 pt-5 md:col-span-2 col-span-2 row-span-1 ">
         Greeting! My name Daniel Dave Meña. A developer based in the
         Philippines. I specialize in building scalable, high-performance web
         applications that prioritize efficiency, usability, and maintainability.
         My approach focuses on clean, efficient code, seamless user experiences,
         and architectures that handle scale without compromising performance.
       </p>
-      <div className="md:col-span-2  md:row-span-1 col-span-2">
+      <div className="md:col-span-2 row-span-1 col-span-2">
         <Line />
         <p className="mt-2 font-bold tracking-widest text-center md:text-[20px] text-[16px]">
           Education
