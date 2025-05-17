836a9d19a81f0dbb868a7fd8428d00904950324a
danieldavemena <danieldanieldanieldave@gmail.com>
2025-05-17 15:42:51 +0800

    modified:   app/app.css 	modified:   app/index/page.tsx 	new file:   public/images/blob-1.svg 	new file:   public/images/blob-2.svg 	new file:   public/images/blob-3.svg 	new file:   public/images/blob-4.svg

--- app/index/page.tsx
+++ app/index/page.tsx
@@ -1,16 +1,33 @@
 import React from "react";
 import Banner from "./components/banner";
 import Arrow from "~/globalcomponents/arrow";
-import Gradient from "~/globalcomponents/gradient";
 import Indicator from "~/globalcomponents/pageindicator";
 import About from "./components/about";
 import Projects from "./components/projects";
-import Line from "~/globalcomponents/line";
 
 const page = () => {
   return (
     <main>
-      <Gradient />
+      <img
+        className="fixed blur-3xl transform md:-translate-x-[15vw] md:-translate-y-[20vh] -translate-x-[20vw]   opacity-70"
+        src="/public/images/blob-1.svg"
+        alt=""
+      />
+      <img
+        className="fixed blur-3xl transform md:translate-x-[60vw] md:translate-y-[20vh] translate-x-[20vw] translate-y-[60vh] opacity-70"
+        src="/public/images/blob-2.svg"
+        alt=""
+      />
+      <img
+        className="fixed blur-3xl transform md:translate-x-[5vw] md:translate-y-[35vh] -translate-x-[50vw] translate-y-[40vh] opacity-20"
+        src="/public/images/blob-3.svg"
+        alt=""
+      />
+      <img
+        className="fixed blur-3xl transform md:translate-x-[40vw] md:-translate-y-[40vh] translate-x-[50vw] translate-y-[20vh] opacity-20"
+        src="/public/images/blob-4.svg"
+        alt=""
+      />
       <Arrow />
       <Indicator />
       <header id="home-section" className="h-screen w-screen relative">
