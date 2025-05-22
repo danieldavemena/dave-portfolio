8ff537bfecfd99c070b5d4d20a8ec56eeb88af2a
danieldavemena <danieldanieldanieldave@gmail.com>
2025-05-22 23:32:05 +0800

    modified:   app/globalcomponents/navigation.tsx 	new file:   app/globalcomponents/projectsList.json 	modified:   app/routes.ts 	new file:   app/routes/projects.tsx 	new file:   public/images/be-my-valentine.png 	new file:   public/images/conference-ticket-generator.png 	new file:   public/images/wpf-calculator.png

--- app/globalcomponents/navigation.tsx
+++ app/globalcomponents/navigation.tsx
@@ -27,11 +27,12 @@ const navigation = () => {
     <div
       className={`fixed flex items-center justify-center ${hide} transition-all ease-in-out duration-300 left-1/2 transform -translate-x-1/2 z-10`}
     >
-      <nav className="absolute flex flex-row justify-center  md:text-xl text-md font-bold bg-[#090c11] md:w-[195px] w-[150px] md:h-[45px] h-[30px] text-[#ffffff92] z-11   rounded-full gap-5 items-center ">
+      <nav className="absolute flex flex-row justify-center  md:text-xl text-md font-bold bg-[#090c11] md:w-[280px] w-[220px] md:h-[45px] h-[30px] text-[#ffffff92] z-11   rounded-full gap-5 items-center ">
         <a href="/">Home</a>
         <a href="about">About</a>
+        <a href="projects">Projects</a>
       </nav>
-      <div className="absolute  bg-gradient-to-tr from-purple-500 to-blue-500 md:w-[200px] w-[155px] md:h-[50px] h-[35px]  rounded-full"></div>
+      <div className="absolute  bg-gradient-to-tr from-purple-500 to-blue-500 md:w-[285px] w-[225px] md:h-[50px] h-[35px]  rounded-full"></div>
     </div>
   );
 };
---
+++ app/routes/projects.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { redirect } from "react-router";
+import Navigation from "~/globalcomponents/navigation";
+import project from "~/globalcomponents/projectsList.json";
+
+const projects = () => {
+  return (
+    <main>
+      <Navigation />
+      <img
+        className="fixed blur-[140px] transform md:-translate-x-[15vw] md:-translate-y-[20vh] -translate-x-[20vw]   opacity-70"
+        src="/images/blob-1.svg"
+        alt=""
+      />
+      <img
+        className="fixed blur-[140px] transform md:translate-x-[60vw] md:translate-y-[20vh] translate-x-[20vw] translate-y-[60vh] opacity-70"
+        src="/images/blob-2.svg"
+        alt=""
+      />
+      <img
+        className="fixed blur-[140px] transform md:translate-x-[5vw] md:translate-y-[35vh] -translate-x-[50vw] translate-y-[40vh] opacity-20"
+        src="/images/blob-3.svg"
+        alt=""
+      />
+      <img
+        className="fixed blur-[140px] transform md:translate-x-[40vw] md:-translate-y-[40vh] translate-x-[50vw] translate-y-[20vh] opacity-20"
+        src="/images/blob-4.svg"
+        alt=""
+      />
+
+      <div className="md:w-[1200px] w-[360px]   absolute left-1/2 top-30 transform -translate-x-1/2">
+        <div className="md:columns-3 columns-1 [&>*:not(:first-child)]:mt-5">
+          {project.projects.map((data, key) => {
+            return (
+              <div
+                onClick={() => redirect(data.webUrl)}
+                key={key}
+                className="bg-[#ffffff16] hover:scale-102 duration-200 transition-all ease-in-out  overflow-hidden w-full rounded-lg  "
+              >
+                <div className="overflow-clip h-[185px]  rounded-t-lg">
+                  <img src={data.imgUrl} alt="" className="object-fill" />
+                </div>
+                <div className="p-2">
+                  <h2 className="font-bold text-[15pt] content">
+                    {data.title}
+                  </h2>
+                  <p className="mt-2 text-[#ffffffac]">☆ {data.description}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default projects;
